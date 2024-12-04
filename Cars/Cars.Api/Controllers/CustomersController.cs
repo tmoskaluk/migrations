293d170bc7d25c30160fb936c4d2d997b8c506af
Tomasz Moskaluk <tmoskaluk@sii.pl>
2024-12-04 13:08:50 +0100

    Customer actions added

--- Cars/Cars.Api/Controllers/CustomersController.cs
+++ Cars/Cars.Api/Controllers/CustomersController.cs
@@ -21,17 +21,44 @@ public class CustomersController(CustomersDbContext context) : ControllerBase
     }
 
     [HttpPost]
-    public void Post([FromBody] Customer value)
+    public IActionResult Create([FromBody] Customer value)
     {
+        var exists = context.Customers.Any(x => x.IdentityNo == value.IdentityNo);
+        if (exists) throw new ArgumentException("Customer with this identity number already exists");
+
+        var customer = context.Customers.Add(value);
+        context.SaveChanges();
+
+        var location = Url.Action(nameof(Create), new { id = customer.Entity.Id }) ?? $"/{customer.Entity.Id}";
+        return Created(location, customer.Entity.Id);
     }
 
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
+    public IActionResult Update(int id, [FromBody] Customer value)
     {
+        var customer = context.Customers.FirstOrDefault(x => x.Id == id);
+        if (customer == null) throw new ArgumentException("Invalid customer identifier");
+
+        var exists = context.Customers.Any(x => x.Id != customer.Id && x.IdentityNo == value.IdentityNo);
+        if (exists) throw new ArgumentException("Customer with this identity number already exists");
+
+        customer.IdentityNo = value.IdentityNo;
+        customer.FirstName = value.FirstName;
+        customer.LastName = value.LastName;
+        customer.Phone = value.Phone;
+        context.SaveChanges();
+
+        return Ok();
     }
 
     [HttpDelete("{id}")]
-    public void Delete(int id)
+    public IActionResult Delete(int id)
     {
+        var customer = context.Customers.FirstOrDefault(x => x.Id == id);
+        if (customer == null) throw new ArgumentException("Invalid customer identifier");
+
+        context.Remove(customer);
+        context.SaveChanges();
+        return NoContent();
     }
 }
